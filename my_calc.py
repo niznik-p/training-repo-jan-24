53acbcbf47e27e9d6f45e522aa905143aee5980c
Mattie Niznik <mattie.niznik@princeton.edu>
2024-01-11 14:18:10 -0500

    Refactored x_len to x_length

--- my_calc.py
+++ my_calc.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-x_len = int(input('How many arrays of 100? '))  # Not the best error handling!
+x_length = int(input('How many arrays of 100? '))  # Not the best error handling!
 y_len = 100
-a = np.random.randint(1, 100, [x_len, y_len])
+a = np.random.randint(1, 100, [x_length, y_len])
 
-for i in range(0, x_len):
+for i in range(0, x_length):
     print(a[i].mean())
