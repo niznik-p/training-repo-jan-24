2878b03421901cf776507df0f5116f9ebaa99a7d
Mattie Niznik <mattie.niznik@princeton.edu>
2024-01-11 12:40:22 -0500

    Fix a bug where the loop didn't adjust to user input

--- my_calc.py
+++ my_calc.py
@@ -4,5 +4,5 @@ x_len = int(input('How many arrays of 100?'))  # Not the best error handling!
 y_len = 100
 a = np.random.randint(1, 100, [x_len, y_len])
 
-for i in range(0, 5):
+for i in range(0, x_len):
     print(a[i].mean())
